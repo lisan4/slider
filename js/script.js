5c59084849bdb0cfc6fd65bf91351ea92af02123
Ivan <mloveannn@mail.ru>
2024-10-13 13:42:58 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-console.log(3333)
-
-// import slider from './modules/slider';
-
-
+import slider from './modules/slider';
 
 window.addEventListener("DOMContentLoaded", () => {
-    console.log(3333)
-    // slider({
-    //     container: ".offer__slider", // общая обертка, куда обернуто всё
-    //     wrapper: ".offer__slider-wrapper", //обертка, чтобы подключить слайдеру overflow
-    //     field: ".offer__slider-inner", //обертка, уже для самих слайдов + чтобы растянуть слайды по горизонту 
-    //     slide: '.offer__slide', // каждый слайд который имеется
-    //     nextArrow: ".offer__slider-next", // кнопка следующего файла
-    //     prevArrow: ".offer__slider-prev", // кнопка предыдущего файла
-    //     totalCounter: "#total", // общее число слайдов
-    //     currentCounter: "#current" // какой по счету открыт слайд прямо сейчас
-    // });
+    
+    slider({
+        container: ".offer__slider", // общая обертка, куда обернуто всё
+        wrapper: ".offer__slider-wrapper", //обертка, чтобы подключить слайдеру overflow
+        field: ".offer__slider-inner", //обертка, уже для самих слайдов + чтобы растянуть слайды по горизонту 
+        slide: '.offer__slide', // каждый слайд который имеется
+        nextArrow: ".offer__slider-next", // кнопка следующего файла
+        prevArrow: ".offer__slider-prev", // кнопка предыдущего файла
+        totalCounter: "#total", // общее число слайдов
+        currentCounter: "#current" // какой по счету открыт слайд прямо сейчас
+    });
 });
